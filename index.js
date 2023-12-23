013a19807617ea20a34604e1656af488c500fe5c
hs <67200805+hs-x11@users.noreply.github.com>
2023-12-23 11:43:16 -0500

    Updated loop in JS

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
     const button = document.querySelector('#randomize-btn');
     button.addEventListener('click', async () => {
         try {
-            const fetchRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
+            const fetchRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=53084');
             let recipe = fetchRecipe.data.meals;
             console.log(recipe);
 
@@ -13,17 +13,29 @@
             }    
 
             let recipeInfo = '';
-            let updateList = document.querySelector('#ing-list')
+            let updateList = document.querySelector('#ingredients-list');
+            let updateInstructions = document.querySelector('#instructions-list');
 
             recipe.forEach(object => {
                 for (let i = 1; i <= 20; i++) {
                     let measure = object[`strMeasure${i}`];
                     let ingredient = object[`strIngredient${i}`];
-  
-                    recipeInfo += `<li>${measure} ${ingredient}</li>`;
+                    let noNullPic = object[`strMealThumb`];
+
+                    if (measure && ingredient && noNullPic !== 'null') {
+                        recipeInfo += `<li>${measure} ${ingredient}</li>`;
+                    } else {
+                        catchError = document.querySelector('.bottom-container');
+                        catchError.textContent = 'This recipe has an error, please click the randomize button to find another!';
+                    }
                 }
+
+                updateInstructions.textContent = object.strInstructions;
             });
             updateList.innerHTML = `<ul>${recipeInfo}</ul>`;
+
+
+        
 
         } catch (error) {
             //Write something here for errors
