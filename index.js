3e54fe68748ea078c4eed1a4e5c6347df5596cd7
hs <67200805+hs-x11@users.noreply.github.com>
2023-12-23 10:05:30 -0500

    Updates to JS and HTML

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 !(async function () { 
 
-    let fetchRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
-    let recipe = fetchRecipe.data.meals;
-    console.log(recipe);
-
-    document.querySelector('.top-container').src = recipe.strMealThumb;
-
     const button = document.querySelector('#randomize-btn');
-    button.addEventListener('click', () => {
-        const unhideDiv = document.querySelector('.bottom-container');
-        if (unhideDiv.classList.contains('hidden')) {
-            unhideDiv.classList.remove('hidden');
-        }    
+    button.addEventListener('click', async () => {
+        try {
+            const fetchRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
+            let recipe = fetchRecipe.data.meals;
+            console.log(recipe);
 
-        const updateList = document.querySelector('#ing-list')
-        updateList.innerHTML = `
-        <ul>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
-        </ul> 
-        `
+            const unhideDiv = document.querySelector('.bottom-container');
+            if (unhideDiv.classList.contains('hidden')) {
+                unhideDiv.classList.remove('hidden');
+            }    
+
+            let recipeInfo = '';
+            let updateList = document.querySelector('#ing-list')
+            recipe.forEach(object => {
+            recipeInfo += `
+                <ul>
+                    <li>${object.strIngredient1}</li>
+                    <li>${object.strIngredient2}</li>
+                    <li>${object.strIngredient3}</li>
+                </ul> 
+                `;
+                updateList.innerHTML = recipeInfo;
+            });
+
+        } catch (error) {
+            //Write something here for errors
+        }
     });
   
 })();
