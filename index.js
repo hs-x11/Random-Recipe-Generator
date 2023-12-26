fb944ed6296d955bfd4de4d63bd4430be803847b
hs <67200805+hs-x11@users.noreply.github.com>
2023-12-26 09:22:14 -0500

    Added updates to all files

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 !(async function () { 
 
+    const recipeFormatting = (recipe) => {
+        let recipeInfo = '';
+        let updateList = document.querySelector('#ingredients-list');
+
+        recipe.forEach((api) => {
+            for (let i = 1; i <= 20; i++) {
+                let measure = api[`strMeasure${i}`];
+                let ingredient = api[`strIngredient${i}`];
+                let noNullPic = api[`strMealThumb`];
+
+                if (measure && ingredient && noNullPic !== 'null') {
+                    recipeInfo += `<li>▸ ${measure} ‐ ${ingredient}</li>`;
+                } 
+            }
+
+            let updateImg = document.querySelector('#recipe-img');
+            updateImg.src = api.strMealThumb;
+
+            let updateRecipeName = document.querySelector('#welcome-title');
+            updateRecipeName.textContent = api.strMeal;
+
+            let updateInstructions = document.querySelector('#instructions-list');
+            updateInstructions.textContent = api.strInstructions;
+
+        });
+        updateList.innerHTML = recipeInfo;
+    };
+
     const submitBtn = document.querySelector('#submit-btn');
     submitBtn.addEventListener('click', async (event) => {
         event.preventDefault(); 
@@ -14,8 +42,6 @@
         const unhideRandomBtn = document.querySelector('.button-container');
         unhideRandomBtn.classList.remove('hidden');
     });
-    
-
 
     const button = document.querySelector('#randomize-btn');
     button.addEventListener('click', async () => {
@@ -30,36 +56,64 @@
             const showcaseDiv = document.querySelector('.recipe-showcase');
             showcaseDiv.classList.add('hidden');
             
+            recipeFormatting(recipe);
 
+        } catch (error) {
+            //Write something here for errors
+        }
+    });
 
-            let recipeInfo = '';
-            let updateList = document.querySelector('#ingredients-list');
+    const pizza = document.querySelector('#pizza');
+    pizza.addEventListener('click', async () => {
+        try {
+            const fetchRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=53014');
+            let recipe = fetchRecipe.data.meals;
 
-            recipe.forEach((api) => {
-                for (let i = 1; i <= 20; i++) {
-                    let measure = api[`strMeasure${i}`];
-                    let ingredient = api[`strIngredient${i}`];
-                    let noNullPic = api[`strMealThumb`];
+            const mainBodyDiv = document.querySelector('.main-body');
+            mainBodyDiv.classList.remove('hidden');
 
-                    if (measure && ingredient && noNullPic !== 'null') {
-                        recipeInfo += `<li>▸ ${measure} ‐ ${ingredient}</li>`;
-                    } 
-                }
+            const showcaseDiv = document.querySelector('.recipe-showcase');
+            showcaseDiv.classList.add('hidden');
+            
+            recipeFormatting(recipe);
 
-                let updateImg = document.querySelector('#recipe-img');
-                updateImg.src = api.strMealThumb;
+        } catch (error) {
+            //Write something here for errors
+        }
+    });
 
-                let updateRecipeName = document.querySelector('#welcome-title');
-                updateRecipeName.textContent = api.strMeal;
+    const biryani = document.querySelector('#biryani');
+    biryani.addEventListener('click', async () => {
+        try {
+            const fetchRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52805');
+            let recipe = fetchRecipe.data.meals;
 
-                let updateInstructions = document.querySelector('#instructions-list');
-                updateInstructions.textContent = api.strInstructions;
+            const mainBodyDiv = document.querySelector('.main-body');
+            mainBodyDiv.classList.remove('hidden');
 
-            });
-            updateList.innerHTML = recipeInfo;
+            const showcaseDiv = document.querySelector('.recipe-showcase');
+            showcaseDiv.classList.add('hidden');
+            
+            recipeFormatting(recipe);
 
+        } catch (error) {
+            //Write something here for errors
+        }
+    });
 
-        
+    const kfChicken = document.querySelector('#kfchicken');
+    kfChicken.addEventListener('click', async () => {
+        try {
+            const fetchRecipe = await axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52813');
+            let recipe = fetchRecipe.data.meals;
+
+            const mainBodyDiv = document.querySelector('.main-body');
+            mainBodyDiv.classList.remove('hidden');
+
+            const showcaseDiv = document.querySelector('.recipe-showcase');
+            showcaseDiv.classList.add('hidden');
+            
+            recipeFormatting(recipe);
 
         } catch (error) {
             //Write something here for errors
