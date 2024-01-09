abd36b855d45d4ac79fb008ea0919843e8e65d92
hs <67200805+hs-x11@users.noreply.github.com>
2024-01-09 14:17:00 -0500

    Testing new format to see if it will work on pages

--- Javascript/recipes-page-formatting.js
+++ Javascript/recipes-page-formatting.js
@@ -38,98 +38,83 @@
 
     const displayRecipe = async () => {
         let recipe = '';
-        
-        switch(window.location.pathname) {
-            case '/list/1.html':
+        const path = window.location.pathname;
+    
+        switch (true) {
+            case path.endsWith('/list/1.html'):
                 recipe = await getRecipe(1);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/2.html':
+            case path.endsWith('/list/2.html'):
                 recipe = await getRecipe(2);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/3.html':
+            case path.endsWith('/list/3.html'):
                 recipe = await getRecipe(3);
                 recipeFormatting(recipe);
                 break;
-                    
-            case '/list/4.html':
+            case path.endsWith('/list/4.html'):
                 recipe = await getRecipe(4);
                 recipeFormatting(recipe);
-                break;  
-
-            case '/list/5.html':
+                break;
+            case path.endsWith('/list/5.html'):
                 recipe = await getRecipe(5);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/6.html':
+            case path.endsWith('/list/6.html'):
                 recipe = await getRecipe(6);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/7.html':
+            case path.endsWith('/list/7.html'):
                 recipe = await getRecipe(7);
                 recipeFormatting(recipe);
                 break;
-                    
-            case '/list/8.html':
+            case path.endsWith('/list/8.html'):
                 recipe = await getRecipe(8);
                 recipeFormatting(recipe);
-                break;  
-            case '/list/9.html':
+                break;
+            case path.endsWith('/list/9.html'):
                 recipe = await getRecipe(9);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/10.html':
+            case path.endsWith('/list/10.html'):
                 recipe = await getRecipe(10);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/11.html':
+            case path.endsWith('/list/11.html'):
                 recipe = await getRecipe(11);
                 recipeFormatting(recipe);
                 break;
-                    
-            case '/list/12.html':
+            case path.endsWith('/list/12.html'):
                 recipe = await getRecipe(12);
                 recipeFormatting(recipe);
-                break;  
-
-            case '/list/13.html':
+                break;
+            case path.endsWith('/list/13.html'):
                 recipe = await getRecipe(13);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/14.html':
+            case path.endsWith('/list/14.html'):
                 recipe = await getRecipe(14);
                 recipeFormatting(recipe);
                 break;
-
-            case '/list/15.html':
+            case path.endsWith('/list/15.html'):
                 recipe = await getRecipe(15);
                 recipeFormatting(recipe);
                 break;
-                    
-            case '/list/16.html':
+            case path.endsWith('/list/16.html'):
                 recipe = await getRecipe(16);
                 recipeFormatting(recipe);
-                break;  
-
-            case '/list/17.html':
+                break;
+            case path.endsWith('/list/17.html'):
                 recipe = await getRecipe(17);
                 recipeFormatting(recipe);
-                break;  
-
-            case '/list/18.html':
+                break;
+            case path.endsWith('/list/18.html'):
                 recipe = await getRecipe(18);
                 recipeFormatting(recipe);
-                break;  
+                break;
+            
         }
-        
     };
     
     displayRecipe();
